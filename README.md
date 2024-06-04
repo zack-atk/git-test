913057dbb56457c87ff3a87f29d8e2d0dea65f5e
zack-atk <zmanning.it@gmail.com>
2024-06-03 18:12:20 -0700

    ADD greetings.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# My first GitHub project
+This is my first GitHub project. I published this directly from VS Code.
---
+++ projects/greetings.py
@@ -0,0 +1,5 @@
+
+# hello
+name = input('Tell me your name, punk: ')
+age = input('...and how old are you? ')
+print('Hi,', age, 'year old',  name + '!')
